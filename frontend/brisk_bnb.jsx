4ca19fa59dfb7d44931958b3bff8326a55e70a49
Andrew Schumacher <as228512@ohio.edu>
2018-05-08 19:48:59 -0400

    general update

--- frontend/brisk_bnb.jsx
+++ frontend/brisk_bnb.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+// import Root from './components/root';
+
 
 
 
@@ -13,6 +16,12 @@ window.signup = signup;
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BriskBnB</h1>, root);
 });
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+
+const dummyUser = Object.freeze({
+  id: null
+});
+
+const sessionReducer = (state = dummyUser, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return { id: action.currentUser.id };
+    case LOGOUT_CURRENT_USER:
+      return dummyUser;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
