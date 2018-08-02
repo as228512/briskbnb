b3e2296cc9db4a38aa4c77d224ccdc93c99ddea6
Andrew Schumacher <as228512@ohio.edu>
2018-08-01 21:45:32 -0400

    trips will now show user's trips in order of startDate, and no dups show

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -19,7 +19,6 @@ const calculateDaysBetweenDates = (startDate, endDate) => {
 
 export const selectBookingsForHome = home => {
   let bookedDates = [];
-  debugger;
   let homeReservationInformation = home.bookingIds.map(
     bookingId => home.bookings[bookingId]
   );
@@ -41,3 +40,35 @@ export const selectBookingsForHome = home => {
 };
 
 export const asArray = ({ homes }) => Object.keys(homes).map(key => homes[key]);
+
+export const asSortedArray = ({ homes, bookings }) => {
+  let orderedBookings = [];
+
+  //extracts dates and home_id from bookings, pushing them into a workable array
+  //of sub-arrays ex. format [[start_date, end_date, home_id],
+  //                          [start_date2, end_date2, home_id2]]
+  Object.keys(bookings).forEach(key => {
+    orderedBookings.push([
+      bookings[key]["start_date"],
+      bookings[key]["end_date"],
+      bookings[key]["home_id"]
+    ]);
+  });
+  // 2 > think about how we would pass the dates of each booking through to be displayed under
+  //     each relevent home (multiple dates for 1 home, for some)
+
+  //sorts by start date, not by order of booking creation / O(n log n) at worst
+  orderedBookings = orderedBookings.sort();
+
+  //itterates over each sorted booking, keys into homes hash via "home_id"
+  //and pushes home objects, in order of start_date, into the result array (orderedTrips)
+  let orderedTrips = new Set();
+  orderedBookings.forEach(booking => {
+    orderedTrips.add(homes[booking[2]]);
+  });
+
+  let orderedTripsArray = Array.from(orderedTrips);
+
+  if (!orderedTripsArray[0]) return [];
+  else return orderedTripsArray;
+};
