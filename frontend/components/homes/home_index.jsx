f8487a726b3f528ac9eddaa58979848edda2aca5
Andrew Schumacher <as228512@ohio.edu>
2018-08-03 20:49:50 -0400

    minor styling to trips page

--- frontend/components/homes/home_index.jsx
+++ frontend/components/homes/home_index.jsx
@@ -4,17 +4,31 @@ import HomeMap from '../home_map/home_map';
 import {withRouter} from 'react-router-dom';
 
 const HomeIndex = (props) => {
+
   const bookings = props.bookings;
   const upcomingTrip = props.upcomingTrip;
 
-  return(
-        <div>
-          <div className='home-index'>
-            {props.homes.map(home => (
-              <HomeIndexItem home={home} bookings={bookings} upcomingTrip={upcomingTrip} key={home.id}/>
-            ))}
-          </div>
-        </div>
-        );};
+  if(props.location.pathname === "/homes") {
+    return(
+            <div>
+              <div className='home-index'>
+                {props.homes.map(home => (
+                  <HomeIndexItem home={home} bookings={bookings} upcomingTrip={upcomingTrip} key={home.id}/>
+                ))}
+              </div>
+            </div>
+    );
+  } else {
+    return (
+             <div>
+               <div className='trip-index'>
+                 {props.homes.map(home => (
+                   <HomeIndexItem home={home} bookings={bookings} upcomingTrip={upcomingTrip} key={home.id}/>
+                 ))}
+               </div>
+             </div>
+    );
+  }
+};
 
 export default withRouter(HomeIndex);
