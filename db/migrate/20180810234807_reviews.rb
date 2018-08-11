8410de9c6b736a58125f45b2aaa61ecd25ba7149
Andrew Schumacher <as228512@ohio.edu>
2018-08-10 20:02:31 -0400

    migrated reviews

---
+++ db/migrate/20180810234807_reviews.rb
@@ -0,0 +1,17 @@
+class Reviews < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reviews do |t|
+      t.text :body, null: false
+      t.integer :rating, null: false
+      t.datetime :created_on, null: false
+
+      t.integer :home_id, null: false
+      t.integer :user_id, null: false
+
+      t.timestamps
+    end
+
+    add_index :reviews, :home_id
+    add_index :reviews, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180613234457) do
+ActiveRecord::Schema.define(version: 20180810234807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,18 @@ ActiveRecord::Schema.define(version: 20180613234457) do
     t.float "long", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "rating", null: false
+    t.datetime "created_on", null: false
+    t.integer "home_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["home_id"], name: "index_reviews_on_home_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
