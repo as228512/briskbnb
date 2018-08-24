b22a4954ededde132b0be759a8b139088f8954ca
Andrew Schumacher <as228512@ohio.edu>
2018-08-23 21:40:48 -0400

    refactoring and progress on a cleaner UI for session error notifications

--- frontend/components/modals/modal_switch.jsx
+++ frontend/components/modals/modal_switch.jsx
@@ -6,38 +6,6 @@ import LoginFormContainer from "../session_form/login_form_container";
 import SignupFormContainer from "../session_form/signup_form_container";
 import ReviewFormContainer from "../reviews/review_form_container";
 
-function Modal(props) {
-  if (!props.modal) {
-    return null;
-  }
-
-  //do we really need the break after each case? *****
-  let component;
-  switch (props.modal) {
-    case "login":
-      component = <LoginFormContainer />;
-      break;
-
-    case "signup":
-      component = <SignupFormContainer />;
-      break;
-
-    case "review":
-      component = <ReviewFormContainer />;
-      break;
-
-    default:
-      return null;
-  }
-  return (
-    <div className="modal-background" onClick={props.closeModal}>
-      <div className="modal-child" onClick={e => e.stopPropagation()}>
-        {component}
-      </div>
-    </div>
-  );
-}
-
 const mapStateToProps = state => {
   return {
     modal: state.ui.modal
@@ -54,3 +22,64 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Modal);
+
+// Modal component below
+//
+//
+//
+//
+//
+//
+//
+//
+
+let component;
+function Modal(props) {
+  if (!props.modal) {
+    return null;
+  }
+
+  //do we really need the break after each case? *****
+  let formName;
+  switch (props.modal) {
+    case "login":
+      component = <LoginFormContainer />;
+      formName = "session";
+      break;
+
+    case "signup":
+      component = <SignupFormContainer />;
+      formName = "session";
+      break;
+
+    case "review":
+      component = <ReviewFormContainer />;
+      formName = "review";
+      break;
+
+    default:
+      return null;
+  }
+
+  return (
+    <div className="modal-background" onClick={props.closeModal}>
+      {renderForm(formName)}
+    </div>
+  );
+}
+
+function renderForm(formName) {
+  if (formName === "session") {
+    return (
+      <div className="session-modal-child" onClick={e => e.stopPropagation()}>
+        {component}
+      </div>
+    );
+  } else if (formName === "review") {
+    return (
+      <div className="review-modal-child" onClick={e => e.stopPropagation()}>
+        {component}
+      </div>
+    );
+  }
+}
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -5,13 +5,13 @@ class SessionForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      e_mail: "",
-      fname: "",
-      lname: "",
+      email: "",
+      firstName: "",
+      lastName: "",
       password: ""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.renderErrors = this.renderErrors.bind(this);
+    // this.renderErrors = this.renderErrors.bind(this);
   }
 
   update(field) {
@@ -27,17 +27,143 @@ class SessionForm extends React.Component {
     this.props.processForm(user).then(this.props.closeModal);
   }
 
-  renderErrors() {
-    return (
-      <ul>
-        {this.props.errors.map((error, i) => (
-          <li className="session-errors" key={`error-${i}`}>
-            {error}
-          </li>
-        ))}
-      </ul>
-    );
+  firstNameInputField() {
+    let firstName;
+
+    if (this.state.firstName === "First Name can't be blank") {
+      firstName = " First Name can't be blank";
+
+      return (
+        <div>
+          <input type="text" value={firstName} className="error-form-field" />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <input
+            type="text"
+            value={this.state.firstName}
+            onChange={this.update("firstName")}
+            className="form-field"
+            placeholder="First Name"
+          />
+        </div>
+      );
+    }
   }
+
+  lastNameInputField() {
+    let lastName;
+
+    if (this.state.lastName === "Last Name can't be blank") {
+      lastName = " Last Name can't be blank";
+
+      return (
+        <div>
+          <input type="text" value={lastName} className="error-form-field" />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <input
+            type="text"
+            value={this.state.lastName}
+            onChange={this.update("lastName")}
+            className="form-field"
+            placeholder="Last Name"
+          />
+        </div>
+      );
+    }
+  }
+
+  emailInputField() {
+    let email;
+
+    if (this.state.email === "E-mail can't be blank") {
+      email = " E-mail can't be blank";
+
+      return (
+        <div>
+          <input type="text" value={email} className="error-form-field" />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <input
+            type="text"
+            value={this.state.email}
+            onChange={this.update("email")}
+            className="form-field"
+            placeholder="Email Address"
+          />
+        </div>
+      );
+    }
+  }
+
+  passwordInputField() {
+    let password;
+
+    if (this.state.password === "Invalid Password (minimum of 6 characters)") {
+      password = " Invalid Password (minimum of 6 characters)";
+
+      return (
+        <div>
+          <input type="text" value={password} className="error-form-field" />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={this.update("password")}
+            className="form-field"
+            placeholder="Create a Password"
+          />
+        </div>
+      );
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let firstName, lastName, email, password;
+
+    if (nextProps.errors.includes("Fname can't be blank")) {
+      firstName = "First Name can't be blank";
+    } else firstName = this.state.firstName;
+
+    if (nextProps.errors.includes("Lname can't be blank")) {
+      lastName = "Last Name can't be blank";
+    } else lastName = this.state.lastName;
+
+    if (nextProps.errors.includes("E mail can't be blank")) {
+      email = "E-mail can't be blank";
+    } else email = this.state.email;
+
+    if (
+      nextProps.errors.includes(
+        "Password is too short (minimum is 6 characters)"
+      )
+    ) {
+      password = "Invalid Password (minimum of 6 characters)";
+    } else password = this.state.password;
+
+    this.setState({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
+    });
+  }
+
+  //clean up the component a bit and make sure to send a clear errors action onClick to the newly made error-form-fields
+  //so, on click, it will clear the field for the user to try again! :D
 
   render() {
     switch (this.props.formType) {
@@ -45,72 +171,43 @@ class SessionForm extends React.Component {
         return (
           <div className="signup-form-container">
             <form onSubmit={this.handleSubmit}>
-              <br />
               <div onClick={this.props.closeModal} className="close-x">
                 X
               </div>
-              {this.renderErrors()}
 
               <div>
                 <div>
+                  {this.emailInputField()}
+
+                  <br />
+
+                  {this.firstNameInputField()}
+
+                  <br />
+
+                  {this.lastNameInputField()}
+
+                  <br />
+
+                  {this.passwordInputField()}
+
+                  <br />
+
                   <input
-                    type="text"
-                    value={this.state.e_mail}
-                    onChange={this.update("e_mail")}
-                    className="form-field"
-                    placeholder="Email Address"
+                    className="signup-session-submit"
+                    type="submit"
+                    value={"Sign up"}
                   />
                 </div>
 
                 <br />
 
-                <div>
-                  <input
-                    type="text"
-                    value={this.state.fname}
-                    onChange={this.update("fname")}
-                    className="form-field"
-                    placeholder="First name"
-                  />
-                </div>
-
-                <br />
-
-                <div>
-                  <input
-                    type="text"
-                    value={this.state.lname}
-                    onChange={this.update("lname")}
-                    className="form-field"
-                    placeholder="Last name"
-                  />
-                </div>
-
-                <br />
-
-                <div>
-                  <input
-                    type="password"
-                    value={this.state.password}
-                    onChange={this.update("password")}
-                    className="form-field"
-                    placeholder="Create a Password"
-                  />
-                </div>
-
-                <br />
-                <input
-                  className="signup-session-submit"
-                  type="submit"
-                  value={"Sign up"}
-                />
+                <div className="signup-form-footer-border" />
+                <span className="sign-up-form-footer">
+                  Already have a Briskbnb account?{" "}
+                  <i className="form-footer-link">{this.props.otherForm} </i>
+                </span>
               </div>
-              <br />
-              <div className="signup-form-footer-border" />
-              <span className="sign-up-form-footer">
-                Already have a Briskbnb account?{" "}
-                <i className="form-footer-link">{this.props.otherForm} </i>
-              </span>
             </form>
           </div>
         );
@@ -119,18 +216,16 @@ class SessionForm extends React.Component {
         return (
           <div className="login-form-container">
             <form onSubmit={this.handleSubmit}>
-              <br />
               <div>
                 <div onClick={this.props.closeModal} className="close-x">
                   X
                 </div>
-                {this.renderErrors()}
 
                 <div>
                   <input
                     type="text"
-                    value={this.state.e_mail}
-                    onChange={this.update("e_mail")}
+                    value={this.state.email}
+                    onChange={this.update("email")}
                     className="form-field"
                     placeholder="Email Address"
                   />
