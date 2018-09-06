53238bd1bd052b6ee36bdf6e8c5a3384c80f853d
Andrew Schumacher <as228512@ohio.edu>
2018-09-06 19:06:12 -0400

    fixing migration/seed null values issue (mid process)

--- db/migrate/20180829223111_add_booking_to_reviews.rb
+++ db/migrate/20180829223111_add_booking_to_reviews.rb
@@ -1,6 +1,6 @@
 class AddBookingToReviews < ActiveRecord::Migration[5.1]
   def change
-    add_column :reviews, :booking_id, :integer
+    add_column :reviews, :booking_id, :integer, null: false
 
     add_index :reviews, :booking_id
   end
